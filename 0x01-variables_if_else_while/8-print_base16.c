762159a5ac7d309cfa7db5a905411b60b3bb53c8
wasanRuntime <andrewwasan@gmail.com>
2023-04-22 11:53:48 +0000

    print base 16 numbers 0123456789abcdef

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - prints all numbers of base 16
+ *
+ * Return: Always return 0
+ */
+int main(void)
+{
+	int num;
+	char letter;
+	num = 0;
+	while (num < 10)
+	{
+		putchar((num % 10) + '0');
+		num++;
+	}
+	for (letter = 'a'; letter <= 'f'; letter++)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+	return 0;
+}
